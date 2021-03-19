b130c5109d6f932f7c3cb0e26ab73cfb9301a169
kweilin123 <guilinw98@gmail.com>
2021-03-19 19:40:40 +0800

    24

---
+++ src/leetcode/ListNode.java
@@ -0,0 +1,25 @@
+package leetcode;
+
+/**
+ * TODO
+ *
+ * @author CodeX
+ * @version 1.0
+ * @date 2021/3/19 17:20
+ */
+public class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    public ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
