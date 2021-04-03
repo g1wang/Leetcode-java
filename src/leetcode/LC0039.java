4ee8f1451e287405395c465fa6b2cbb505e248e8
kweilin123 <guilinw98@gmail.com>
2021-04-04 07:14:06 +0800

    lc

---
+++ src/leetcode/LC0039.java
@@ -0,0 +1,31 @@
+package leetcode;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/*
+Combination Sum
+ */
+public class LC0039 {
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>> retList = new ArrayList<>();
+        List<Integer> list = new ArrayList<>();
+        Arrays.sort(candidates);
+        combination(candidates, target, 0, list, retList);
+        return retList;
+    }
+
+    private void combination(int[] candidates, int target, int s, List<Integer> list, List<List<Integer>> retList) {
+        if (target == 0) {
+            retList.add(new ArrayList<>(list));
+            return;
+        }
+        for (int i = s; i < candidates.length; i++) {
+            if (target < candidates[i]) break;
+            list.add(candidates[i]);
+            combination(candidates, target - candidates[i], i, list, retList);
+            list.remove(list.size() - 1);
+        }
+    }
+}
