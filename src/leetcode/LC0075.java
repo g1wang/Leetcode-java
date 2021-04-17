c6bb671c49fc58d24f472c6c240bf7a0f86d28a8
kweilin123 <guilinw98@gmail.com>
2021-04-17 22:57:57 +0800

    LC

---
+++ src/leetcode/LC0075.java
@@ -0,0 +1,45 @@
+package leetcode;
+
+/**
+ * Sort Colors
+ *
+ * @author CodeX
+ * @version 1.0
+ * @date 2021/4/17 21:28
+ */
+public class LC0075 {
+    public void sortColors(int[] nums) {
+        //O(nlogn)
+        //Arrays.sort(nums);
+
+        //O(n)
+        int end = nums.length - 1;
+        int start = 0;
+        int i = 0;
+        while (start < end && i <= end) {
+            if (nums[i] == 0) {
+                nums[start] = 0;
+                if (i > start) {
+                    nums[i] = 1;
+                }
+                start++;
+                i++;
+                continue;
+            }
+            if (nums[i] == 1) {
+                i++;
+                continue;
+            }
+            if (nums[i] == 2) {
+                int tmp = nums[end];
+                nums[i] = tmp;
+                nums[end] = 2;
+                end--;
+            }
+
+        }
+
+
+    }
+}
+
