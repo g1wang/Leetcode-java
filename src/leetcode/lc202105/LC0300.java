173dfe7c68c04ba88cc48de9c76fb89d60d874e6
kweilin123 <guilinw98@gmail.com>
2021-05-05 07:38:55 +0800

    LC

---
+++ src/leetcode/lc202105/LC0300.java
@@ -0,0 +1,26 @@
+package leetcode.lc202105;
+
+/**
+ * 300. Longest Increasing Subsequence
+ * 幽游白书
+ * @author CodeX
+ * @version 1.0
+ * @date 2021/5/4
+ */
+public class LC0300 {
+    public int lengthOfLIS(int[] nums) {
+        int max = 0;
+        int[] countArr = new int[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i-1; j >=0; j--) {
+                if (nums[j] < nums[i]) {
+                    countArr[i] = Math.max(countArr[j]+1,countArr[i]);
+                }
+            }
+            countArr[i] = Math.max(countArr[i],1);
+            max = Math.max(countArr[i], max);
+        }
+        return max;
+    }
+
+}
